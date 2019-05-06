84a56e5d5c3c4ab7273da386e5636f77521535ce
mirayyub <mirayyubab@code.edu.az>
2019-05-07 03:02:34 +0400

    add pulse animation

--- eduglobal/js/main.js
+++ eduglobal/js/main.js
@@ -1,87 +1,98 @@
 
 
-// Alert close start
+
 $(document).ready(function () {
+  
+  // Alert close start
   $(".alert-close i").click(function () {
     $(".alert").hide();
   });
-});
-
-// Alert close end
 
 
-
-// Modal open start
-$('#myModal').on('shown.bs.modal', function () {
-  $('#myInput').trigger('focus')
-})
-// Modal open end
+  // Alert close end
 
 
 
-// Slider carusel start
-$('.carousel').carousel({
-  interval: 20000
-})
-// Slider carusel end
+  // Modal open start
+  $('#myModal').on('shown.bs.modal', function () {
+    $('#myInput').trigger('focus')
+  })
+  // Modal open end
 
 
 
-// Owl carusel start
-$('.owl-carousel').owlCarousel({
-  rtl: false,
-  loop: true,
-  margin: 10,
-  nav: false,
-  dots: false,
-  responsive: {
-    0: {
-      items: 1
-    },
-    600: {
-      items: 3
-    },
-    1000: {
-      items: 5
+  // Slider carusel start
+  $('.carousel').carousel({
+    interval: 20000
+  })
+  // Slider carusel end
+
+
+
+  // Owl carusel start
+  $('.owl-carousel').owlCarousel({
+    rtl: false,
+    loop: true,
+    margin: 10,
+    nav: false,
+    dots: false,
+    responsive: {
+      0: {
+        items: 1
+      },
+      600: {
+        items: 3
+      },
+      1000: {
+        items: 5
+      }
     }
-  }
-})
-// Owl carusel end
+  })
+  // Owl carusel end
 
 
 
 
-//  Isotop gallery start 
-$('.gallery-item').isotope({
-  itemSelector: '.item'
-});
-$('ul li').click(function () {
-  $('ul li').removeClass('active');
-  $(this).addClass('active');
-
-  var sel = $(this).attr('data-filter');
+  //  Isotop gallery start 
   $('.gallery-item').isotope({
-    filter: sel
+    itemSelector: '.item'
   });
+  $('ul li').click(function () {
+    $('ul li').removeClass('active');
+    $(this).addClass('active');
+
+    var sel = $(this).attr('data-filter');
+    $('.gallery-item').isotope({
+      filter: sel
+    });
+  });
+  //  Isotop gallery end
+
+
+
+
+
+
+  // Aos animation start
+  AOS.init();
+
+  // Aos animation end
+
+
+  // Counter up start
+  // jQuery(document).ready(function( $ ) {
+  //   $('.counter').counterUp({
+  //       delay: 10,
+  //       time: 1000
+  //   });
+  // });
+  // Counter up end
+
+  
+  $('#show-hidden-menu').click(function() {
+    $('.hidden-menu').slideToggle("slow");
+    // Alternative animation for example
+    // slideToggle("fast");
+  });
+
 });
-//  Isotop gallery end
-
-
-
-
-
-
-// Aos animation start
-AOS.init();
-
-// Aos animation end
-
-
-// Counter up start
-// jQuery(document).ready(function( $ ) {
-//   $('.counter').counterUp({
-//       delay: 10,
-//       time: 1000
-//   });
-// });
-// Counter up end
