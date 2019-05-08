d8b1a145711cb18b08b4e49ecf5306600ef7f50b
mirayyub <mirayyubab@code.edu.az>
2019-05-08 04:13:33 +0400

    index page updeted , add responsiv to pages, add new pages about ,course,event

--- eduglobal/js/main.js
+++ eduglobal/js/main.js
@@ -2,7 +2,7 @@
 
 
 $(document).ready(function () {
-  
+
   // Alert close start
   $(".alert-close i").click(function () {
     $(".alert").hide();
@@ -50,6 +50,9 @@ $(document).ready(function () {
   })
   // Owl carusel end
 
+  
+
+
 
 
 
@@ -73,6 +76,8 @@ $(document).ready(function () {
 
 
 
+
+
   // Aos animation start
   AOS.init();
 
@@ -88,50 +93,21 @@ $(document).ready(function () {
   // });
   // Counter up end
 
-  
-  $('#show-hidden-menu').click(function() {
+
+  $('#show-hidden-menu').click(function () {
     $('.hidden-menu').slideToggle("slow");
     // Alternative animation for example
     // slideToggle("fast");
   });
 
 
-  var a = 0;
-$(window).scroll(function() {
 
-  var oTop = $('#counter').offset().top - window.innerHeight;
-  if (a == 0 && $(window).scrollTop() > oTop) {
-    $('.counter-value').each(function() {
-      var $this = $(this),
-        countTo = $this.attr('data-count');
-      $({
-        countNum: $this.text()
-      }).animate({
-          countNum: countTo
-        },
-
-        {
-
-          duration: 2000,
-          easing: 'swing',
-          step: function() {
-            $this.text(Math.floor(this.countNum));
-          },
-          complete: function() {
-            $this.text(this.countNum);
-            //alert('finished');
-          }
-
-        });
-    });
-    a = 1;
-  }
-
-});  
-    
-    
-  
-  
 
 
 });
+
+
+
+
+
+
