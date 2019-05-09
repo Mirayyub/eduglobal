dd7f43f6d4febedd75665ec6acb51c9c7c346f1f
mirayyub <mirayyubab@code.edu.az>
2019-05-09 19:24:26 +0400

    add course detail page full , update other bug

--- eduglobal/js/main.js
+++ eduglobal/js/main.js
@@ -106,10 +106,6 @@ $(document).ready(function () {
   });
 
 
-  jQuery(window).load(function () { // makes sure the whole site is loaded
-    jQuery("#status").fadeOut(); // will first fade out the loading animation
-    jQuery("#preloader").delay(1000).fadeOut("slow"); // will fade out the white DIV that covers the website.
-  })
 
 
 });
