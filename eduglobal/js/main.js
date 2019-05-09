4d7fc1d18cf4e9bb4486c5790f8b95b90b1b261a
mirayyub <mirayyubab@code.edu.az>
2019-05-09 23:51:12 +0400

    navbar menu fixed with js

--- eduglobal/js/main.js
+++ eduglobal/js/main.js
@@ -12,7 +12,7 @@ $(document).ready(function () {
   // Alert close end
 
 
-//////////////////////////////////////////////////////////////////////////////////////////////////////
+  //////////////////////////////////////////////////////////////////////////////////////////////////////
 
   // Modal open start
   $('#myModal').on('shown.bs.modal', function () {
@@ -20,7 +20,7 @@ $(document).ready(function () {
   })
   // Modal open end
 
-//////////////////////////////////////////////////////////////////////////////////////////////////////
+  //////////////////////////////////////////////////////////////////////////////////////////////////////
 
   // Slider carusel start
   $('.carousel').carousel({
@@ -28,7 +28,7 @@ $(document).ready(function () {
   })
   // Slider carusel end
 
-//////////////////////////////////////////////////////////////////////////////////////////////////////
+  //////////////////////////////////////////////////////////////////////////////////////////////////////
 
   // Owl carusel start
   $('.owl-carousel').owlCarousel({
@@ -57,7 +57,7 @@ $(document).ready(function () {
 
 
 
-//////////////////////////////////////////////////////////////////////////////////////////////////////
+  //////////////////////////////////////////////////////////////////////////////////////////////////////
 
 
 
@@ -80,7 +80,7 @@ $(document).ready(function () {
 
 
 
-//////////////////////////////////////////////////////////////////////////////////////////////////////
+  //////////////////////////////////////////////////////////////////////////////////////////////////////
 
 
   // Aos animation start
@@ -88,51 +88,67 @@ $(document).ready(function () {
 
   // Aos animation end
 
-//////////////////////////////////////////////////////////////////////////////////////////////////////
- 
-
-  $('#show-hidden-menu').click(function () {
-    $('.hidden-menu').slideToggle("slow");
-   
-  });
+  //////////////////////////////////////////////////////////////////////////////////////////////////////
 
 
-//////////////////////////////////////////////////////////////////////////////////////////////////////
+  
+
+
+  //////////////////////////////////////////////////////////////////////////////////////////////////////
 
   // Time countdown start 
-// Set the date we're counting down to
-var countDownDate = new Date("Jan 5, 2021 15:37:25").getTime();
+  // Set the date we're counting down to
+  var countDownDate = new Date("Jan 5, 2021 15:37:25").getTime();
 
-// Update the count down every 1 second
-var x = setInterval(function() {
+  // Update the count down every 1 second
+  var timerx = setInterval(function () {
 
-  // Get todays date and time
-  var now = new Date().getTime();
-    
-  // Find the distance between now and the count down date
-  var distance = countDownDate - now;
-    
-  // Time calculations for days, hours, minutes and seconds
-  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-    
-  // Output the result in an element with id="demo"
-  document.getElementById("demo").innerHTML = days + " : " + hours + " : "
-  + minutes + " : " + seconds + "  ";
-    
-  // If the count down is over, write some text 
-  if (distance < 0) {
-    clearInterval(x);
-    document.getElementById("demo").innerHTML = "EXPIRED";
-  }
-}, 1000);
+    // Get todays date and time
+    var now = new Date().getTime();
+
+    // Find the distance between now and the count down date
+    var distance = countDownDate - now;
+
+    // Time calculations for days, hours, minutes and seconds
+    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+    // Output the result in an element with id="demotime"
+    document.querySelector("#demotime").innerHTML = " " + days + " : " + hours + " : "
+      + minutes + " : " + seconds + " ";
+
+    // If the count down is over, write some text 
+    if (distance < 0) {
+      clearInterval(timerx);
+      document.getElementById("demotime").innerHTML = "EXPIRED";
+    }
+  }, 1000);
 
 
   // Time countdown end
-//////////////////////////////////////////////////////////////////////////////////////////////////////
+  //////////////////////////////////////////////////////////////////////////////////////////////////////
 
+
+ // navbar scroll on top start //
+  window.onscroll = function () { myFunction() };
+
+  var header = document.getElementById("header-navbar");
+  var sticky = header.offsetTop;
+
+  function myFunction() {
+    if (window.pageYOffset > sticky) {
+      header.classList.add("fixed-top");
+    } else {
+      header.classList.remove("fixed-top");
+    }
+  }
+
+
+  // navbar scroll on top end //
+
+  ////////////////////////////////////////////////////////////////////////////////////////////////////////
 });
 
 
